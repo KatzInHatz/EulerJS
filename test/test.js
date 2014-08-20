2fd80c3f723575141405b6b05b0213850d1863c4
Aaron Ward <aaronward@Aarons-MacBook-Air.local>
2014-08-19 22:27:08 -0700

    add TODOs for unit tests

--- test/test.js
+++ test/test.js
@@ -4,16 +4,11 @@ var validate = require('../lib/utils/validator');
 var find = require('../lib/utils/finder');
 var get = require('../lib/utils/getter');
 
-// generate.file(function(prompt) {
-//   console.log('success');
-// }, function() {
-//   console.log('failure');
-// }, 'coffee', '226');
-
 describe('Generator', function(){
+
   describe('* generate preview', function(){
 
-    it('should generate preview for prompt 001', function(){
+    it('should generate preview for a prompt that exists', function(){
       var answer, solution;
 
       solution = 
@@ -34,7 +29,7 @@ describe('Generator', function(){
       answer.should.equal(solution);
     });
 
-    it('should call failure callback as the prompt for 000', function(){
+    it('should call failure callback on request for prompt below lowest number', function(){
       var test;
 
       generate.preview(function(prompt) {
@@ -47,7 +42,7 @@ describe('Generator', function(){
       test.should.be.true;
     });
 
-    it('should call failure callback as the prompt for 405', function(){
+    it('should call failure callback on request for prompt above maximum number', function(){
       var test;
 
       generate.preview(function(prompt) {
@@ -62,9 +57,9 @@ describe('Generator', function(){
 
   });
 
-  describe('* generate file', function(){
+  xdescribe('* generate file', function(){
 
-    it('should generate a js style template for prompt 001', function(){
+    it('should generate a js style template for a prompt that exists', function(){
       var answer, solution;
 
       solution = 
@@ -87,7 +82,7 @@ describe('Generator', function(){
       answer.should.equal(solution);
     });
 
-  it('should generate a coffeescript style template for prompt 001', function(){
+    it('should generate a coffeescript style template for a prompt that exists', function(){
       var answer, solution;
 
       solution = 
@@ -109,14 +104,163 @@ describe('Generator', function(){
 
       answer.should.equal(solution);
     });
+
+    it('should call failure callback on request to generate prompt numbered below minimum', function(){
+
+    });
+  
+    it('should call failure callback on request to generate prompt numbered above maximum', function(){
+  
+    });
+
   });
 
 });
 
+xdescribe('Validator', function(){
+
+  describe('* validate one', function(){
+
+    it('should validate a javascript answer against its solution', function() {
+      validate.one(function(answer, solution, number) {
+
+      }, function() {
+
+      }, 'js', '001');
+    });
+
+    it('should validate a coffeescript answer against its solution', function() {
+      validate.one(function(answer, solution, number) {
+
+      }, function() {
+
+      }, 'coffee', '001');
+    });
+
+    it('should call failure callback if file does not exist', function() {
+      validate.one(function(answer, solution, number) {
+
+      }, function() {
+
+      }, 'coffee', '000');
+    });
+
+    it('should call failure callback if solution does not exist', function() {
+      validate.one(function(answer, solution, number) {
+
+      }, function() {
+
+      }, 'coffee', '000');
+    });
+
+  });
+
+  describe('* validate all', function(){
+
+    it('should validate all answers against their solution', function() {
+      validate.all(function(answers) {
+
+      }, function() {
+
+      });
+    });
+
+    it('should validate all javascript answers against their solution', function() {
+      validate.all(function(answers) {
+
+      }, function() {
+
+      }, 'js');
+    });
+
+    it('should validate all coffeescript answers against their solution', function() {
+      validate.all(function(answers) {
+
+      }, function() {
+
+      }, 'coffee');
+    });
+
+  });
+
+});
+
+xdescribe('Finder', function() {
+
+  describe('* find all', function(){
+
+    it('should return an array of all solution  files', function(){
+      find.all();
+    });
+
+    it('should return an array of all javascript solution files', function(){
+      find.all('js');
+    });
+
+    it('should return an array of all coffeescript solution files', function(){
+      find.all('coffee');
+    });
+
+    it('should return an array of all solution files for a given number', function(){
+      find.all(undefined, '001');
+    });
+
+    it('should return an array containing only a specified javascript file', function(){
+      find.all('js', '001');
+    });
+
+    it('should return an array containing only a specified coffeescript file', function(){
+      find.all('coffee', '001');
+    });
+
+    it('should return an empty array if no solution files files exist', function() {
+      find.all('');
+    });
+
+  });
+
+  describe('* find highest', function(){
+
+    it('should find highest solution file number', function() {
+      find.highest(function(number) {
+
+      }, function() {
+
+      });
+    });
+
+    it('should find highest coffeescript solution file number', function() {
+      find.highest(function(number) {
+
+      }, function() {
+
+      }, 'coffee');
+    });
+
+    it('should find highest javascript solution file number', function() {
+      find.highest(function(number) {
+
+      }, function() {
+
+      });
+    }, 'js');
+
+    it('should return 000 if no solution files exist', function() {
+      find.highest(function(number) {
+
+      }, function() {
+
+      });
+    });
+
+  });
+});
+
 describe('Getter', function(){
+
   describe('* get solution', function(){
 
-    it('should return \'142913828922\' as the solution for prompt 010', function(){
+    it('should return the solution for a given prompt', function(){
       var answer;
 
       get.solution(function(solution) {
@@ -128,7 +272,7 @@ describe('Getter', function(){
       answer.should.equal('142913828922');
     });
 
-    it('should call failure callback as the solution for prompt 000', function(){
+    it('should call failure callback as the solution for a file that is numbered below the minimum', function(){
       var test;
 
       get.solution(function(solution) {
@@ -140,7 +284,7 @@ describe('Getter', function(){
       test.should.be.true;
     });
 
-    it('should call failure callback as the solution for prompt 405', function(){
+    it('should call failure callback as the solution for a file that is numbered above the maximum', function(){
       var test;
 
       get.solution(function(solution) {
@@ -159,7 +303,11 @@ describe('Getter', function(){
     it('should evaluate coffeescript functions', function() {
       var script, answer;
 
-      script = 'script = ->\n\t100\n\nreturn do script';
+      script = 'script = ->\n' +
+                 '\t100\n' +
+               '\n' +
+               'return do script';
+
       answer = get.answer(script, 'coffee');
 
       answer.should.equal('100');
@@ -168,8 +316,12 @@ describe('Getter', function(){
     it('should evaluate javascript functions', function() {
       var script, answer;
 
-      script = 'return 100';
-      answer = get.answer(script, 'coffee');
+      script = 'var script = function() {\n' + 
+                  '\treturn 100;\n' + 
+                '};\n' +
+                'return script();';
+
+      answer = get.answer(script, 'js');
 
       answer.should.equal('100');
     });
